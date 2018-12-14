ff1f3165e33e9e53193e102abeba88d6f886fda0
Benjamin Holmqvist <getula.californiae@outlook.com>
2018-12-14 23:33:36 +0100

    first online version

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ var auth = require('./auth.json');
 
 //configure logger settings 
 logger.remove(logger.transports.Console);
-logger.add(logger.transports.Console, {
+logger.add(new logger.transports.Console, {
     colorize: true
 });
 logger.level = 'debug';
