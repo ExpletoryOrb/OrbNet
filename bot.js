8031b51e1ab5160c8829241b03120176d86145d5
Benjamin Holmqvist <getula.californiae@outlook.com>
2018-12-16 22:21:47 +0100

    udate
    local var[] and removed "reply" usage

--- bot.js
+++ bot.js
@@ -2,9 +2,23 @@ const Discord = require('Discord.js');
 const client = new Discord.Client();
 
 const config = require('./config.json');
+const {prefix,token} = require('./config.json');
+
+var channelIdList = [
+    {name: 'introductions', id: '445985208240570369'},
+    {name: 'mtg-general', id: '291262363569618944'},
+    {name: 'off-topic', id: '445980791156113438'},
+    {name: 'judge-questions', id: '489477243974844437'},
+    {name: 'deckhelp', id: '445982373591515146'},
+    {name: 'mtg-arena', id: '497183549330292736'},
+    {name: 'videogames', id: '445980818175819806'},
+    {name: 'arguments', id: '445981701332795413'},
+    {name: 'scryfall', id: '452179984073883650'},
+    {name: 'orbnet', id: '523283864169480230'}
+];
 
 
-client.login(config.token);
+client.login(token);
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
@@ -12,18 +26,13 @@ client.on('ready', () => {
    
 
 client.on('message', msg => {
-    if(msg.content === '+ping'){
-        msg.reply('pong!');
-    }
-});
-
-client.on('message',msg => {
     if(!msg.guild.available){return;}
 
-    console.log(msg.channel.channel_id);
+    if((msg.content.includes('[[') || msg.content.includes('!card')) && !(msg.content.includes('_')) && msg.channel.id == getChannelIdByName('mtg-general')){
 
-    if(msg.content === '+info' && msg.channel_id == getChannelIdByName('orbnet')){
-        msg.reply('msg sent from channel: '+msg.channel);
+        msg.channel.send('Please use the '+getChannelName('scryfall')+' channel instead when looking up cards');
+    }else if(msg.content === `${prefix}ping`){
+        msg.channel.send('pong!');
     }
 });
 
@@ -36,17 +45,9 @@ client.on('guildMemberAdd', member => {
 */
  
 function getChannelIdByName(pString){
-    var channelIdList = [
-        {name: 'introductions', id: '445985208240570369'},
-        {name: 'mtg-general', id: '291262363569618944'},
-        {name: 'off-topic', id: '445980791156113438'},
-        {name: 'judge-questions', id: '489477243974844437'},
-        {name: 'deckhelp', id: '445982373591515146'},
-        {name: 'mtg-arena', id: '497183549330292736'},
-        {name: 'videogames', id: '445980818175819806'},
-        {name: 'arguments', id: '445981701332795413'},
-        {name: 'orbnet', id: '523283864169480230'}
-    ];
-
     return channelIdList.find(obj => obj.name === pString).id
+};
+
+function getChannelName(pString){
+    return channelIdList.find(obj => obj.name === pString).name
 };
